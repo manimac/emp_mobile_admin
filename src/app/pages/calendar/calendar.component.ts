aa49fb9fe05b6d43596700d28bc6942e73de73e4
taruneeth <srikrish7.1991@gmail.com>
2023-11-20 21:33:00 +0530

    Calender fixes updated

--- src/app/pages/calendar/calendar.component.ts
+++ src/app/pages/calendar/calendar.component.ts
@@ -5,7 +5,9 @@ import { HttpRequestService } from 'src/app/services/http-request/http-request.s
 import { StorageService } from 'src/app/services/storage/storage.service';
 import { CalendarOptions } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
 // import interactionPlugin from '@fullcalendar/angular';
+declare var $: any;
 
 @Component({
   selector: 'app-calendar',
@@ -13,9 +15,11 @@ import dayGridPlugin from '@fullcalendar/daygrid';
   styleUrls: ['./calendar.component.css']
 })
 export class CalendarComponent implements OnInit {
-  Events: any[] = [];
+  Events: any[] = [
+    { title: 'Meeting', start: '2023-11-15', end: '2023-11-20' }
+  ];
   calendarOptions: CalendarOptions = {
-    plugins: [dayGridPlugin],
+    plugins: [dayGridPlugin, interactionPlugin],
     initialView: 'dayGridMonth',
     headerToolbar: {
       left: 'prev,next today',
@@ -27,22 +31,24 @@ export class CalendarComponent implements OnInit {
     selectable: true,
     selectMirror: true,
     dayMaxEvents: true,
-    eventClick: this.onDateClick.bind(this)
+    // eventClick: this.onDateClick.bind(this),
+    dateClick: this.onDateClick.bind(this),
   };
   onDateClick(res: any) {
-    alert('Clicked on event : ' + res.event.title + " " + res.event.extendedProps.workdate);
+    console.log(res);
+    $('#myModal').modal('show');
+    // alert('Clicked on event : ' + res.event.title + " " + res.event.extendedProps.workdate);
   }
   ngOnInit() {
     setTimeout(() => {
       this.calendarOptions = {
         initialView: 'dayGridMonth',
         events: this.Events,
-        // dateClick: this.onDateClick.bind(this),
       };
     }, 2500);
   }
   dataLists: any = {};
-  constructor(private http: HttpRequestService){
+  constructor(private http: HttpRequestService) {
     this.loadData();
   }
 
@@ -52,13 +58,13 @@ export class CalendarComponent implements OnInit {
     }
     this.http.post('order/staff-transport-requests', params).subscribe(
       (response: any) => {
-        if(response && Array.isArray(response) && response.length>0){
-          response.forEach((el: any)=>{
+        if (response && Array.isArray(response) && response.length > 0) {
+          response.forEach((el: any) => {
             el.start = el.workdate;
           })
           this.Events = response
         }
-        
+
       },
       (error: any) => {
         this.http.exceptionHandling(error);
